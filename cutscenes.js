6c3d3660c175063001457331d422b2ba06700ced
aaryavpatel1-arch <aaryavpatel1@gmail.com>
2026-07-31 13:10:07 +1000

    Clean up cutscenes.js by removing old comments
    Removed comments for completed training and chore phases.

--- cutscenes.js
+++ cutscenes.js
@@ -18,20 +18,17 @@ window.addEventListener('DOMContentLoaded', () => {
   setCaptainSpeech("Welcome aboard, boy! Go hit that dummy 5 times!");
   showBanner("Click on the practice dummy to attack!", 4000);
 
-  // Dummy Training Complete
   callbacks.onDummyComplete = () => {
     setCaptainSpeech("Good swings! Now clean up those 3 crates on deck!");
     showBanner("Walk up to the 3 crates and press 'E' to clean them up.", 5000);
   };
 
-  // Chores Complete -> Storm Crash
   callbacks.onChoresComplete = () => {
     setCaptainSpeech("STORM COMING! HOLD ON TO SOMETHING!");
     showBanner("A massive wave hits the ship!", 3000);
     triggerStorm(3000);
   };
 
-  // Ship Crashes into Dungeon
   callbacks.onShipCrash = () => {
     setCaptainSpeech(null);
     setPhase('arena');
